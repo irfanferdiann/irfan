63d25eef4b4112a276fb880bf2a588119a7377a6
irfanferdiann <90363259+irfanferdiann@users.noreply.github.com>
2021-12-15 21:32:13 +0700

    projekdins
    project ini adalah projectwork

---
+++ about_user.php
@@ -0,0 +1,106 @@
+<?php  
+    session_start();
+      if(!isset($_SESSION['login_user'])) {
+        header("location: login.php");
+      }else{
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <link rel="stylesheet" type="text/css" href="fontawesome/css/all.min.css">
+
+    <title>User Page</title>
+  </head>
+  <body>
+
+  <!-- Jumbotron -->
+      <div class="jumbotron jumbotron-fluid text-center">
+        <div class="container">
+          <h1 class="display-4"><span class="font-weight-bold">DIN'S RESTO`</span></h1>
+          <hr>
+          <p class="lead font-weight-bold">Pilih <strong>MENU</strong>rut Favorit Kamu <br> 
+          Enjoy Your Food</p>
+        </div>
+      </div>
+  <!-- Akhir Jumbotron -->
+  <!-- Navbar -->
+      <nav class="navbar navbar-expand-lg  bg-dark">
+        <div class="container">
+        <a class="navbar-brand text-white" href="user.php"><strong>DIN'S</strong> RESTO</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link mr-4" href="user.php">HOME</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link mr-4" href="about_user.php">ABOUT</a>
+            </li>
+             <li class="nav-item">
+              <a class="nav-link mr-4" href="pesanan_pembeli.php">CART</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link mr-4" href="logout.php">LOGOUT</a>
+            </li>
+          </ul>
+        </div>
+       </div> 
+      </nav>
+  <!-- Akhir Navbar -->
+
+   <!-- Menu -->    
+      <div class="container">
+        <div class="judul text-center mt-5">
+            <img src="image/dinss.png">
+            <h3 class="font-weight-bold">DINS RESTO</h3>
+        </div>
+
+        <p><h4>ANGGOTA</h4></p>        <p>
+          <h6>1. IRFAN FERDIANSYAH XII TEL 10</h6></p>
+        <p><h6>2. MOHAMMAD NAUFAL WIDI PRATAMA XII TEL 12</h6></p>
+      </div>
+  <!-- Akhir Menu -->
+
+  <!-- Awal Footer -->
+      <hr class="footer">
+      <div class="container">
+        <div class="row footer-body">
+          <div class="col-md-6">
+          <div class="copyright">
+            <strong>Copyright</strong> <i class="far fa-copyright"></i> 2021 -  DIN'S RESTO</p>
+          </div>
+          </div>
+
+          <div class="col-md-6 d-flex justify-content-end">
+          <div class="icon-contact">
+           <label class="font-weight-bold">Chat In Here </label>
+          <a href=http://wa.me/+6281383726008><img src="image/wa.png" class="mr-3" data-toggle="tooltip" title="Whatsapp"></a>
+        </div>
+          </div>
+        </div>
+      </div>
+  <!-- Akhir Footer -->
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/jquery.js"></script>
+  </body>
+</html>
+<?php } ?>
